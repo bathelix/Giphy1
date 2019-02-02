d6657f007480c50e0f7fdb20a19de99249b3f90e
BatHelix <bathelix@gmail.com>
2019-02-01 21:44:43 -0500

    new css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log("connect");
 
 //the array for the gifs to get connected to.
 
-var animals = ["dog","cat","lizard","fish"]
+var animals = ["Dog","Cat","Lizard","Fish"]
 
 for (var i=0; i<animals.length; i++) {
 var x =$("<button>");
@@ -19,7 +19,9 @@ $("#animals").append(x);
 
 $(document).on('click', '.animal', function(){
     console.log("aniumal was clicked");
-    var name = $(this).attr("data-type")
+    var name = $(this).attr("data-type");
+    console.log(this);
+    console.log("NAME: ", name)
     var query = "http://api.giphy.com/v1/gifs/search?q=" + name + 
     "&api_key=F23l80vzJW7GhUpjNIbByxNhXsv2pZe3"
     //<img src="googhle.com">
@@ -27,14 +29,15 @@ $(document).on('click', '.animal', function(){
     $.ajax({
         url:query,
         method:"GET"
-    }).then(function(response){
-        console.log(response)
+    })
+    .then(function(response){
+        console.log("RESPONSE FROM API: ",response)
         var results = response.data;
 
         for (var i=0; i<results.length; i++) {
             var animalImg = $("<img>");
             animalImg.attr("src", results[i].images.fixed_width.url)
-            console.log(results[i].url)
+            //console.log(results[i].url)
             // drag id in html .append animalimG
             $("#gifs").append(animalImg);
 
